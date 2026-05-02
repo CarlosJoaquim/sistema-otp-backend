323bb32afd00bb671588879f899305377a553be7
CarlosJoaquim <carlosdanilojoaquim1005@gmail.com>
2026-05-02 03:01:57 +0100

    Add Next.js web-admin frontend

--- web-admin/next.config.js
+++ web-admin/next.config.js
@@ -1,11 +1,9 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:3000/api/:path*'
-      }
-    ]
-  }
+  images: {
+    unoptimized: true,
+  },
 }
+
+module.exports = nextConfig
